669989304549d2464adc1ac333e2356c36c38e14
Daniel <dgarwood@uncc.edu>
2020-05-02 15:27:18 -0400

    Allow cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
+
 const app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
